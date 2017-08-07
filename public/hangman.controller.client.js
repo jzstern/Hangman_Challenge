42e46e7a2fde4e20616b4ca2bd4cd32f20a71c4b
Jeremy Stern <jzstern@gmail.com>
2017-08-06 20:18:52 -0400

    bug fixes (:
    just need to add network shiz

--- public/hangman.controller.client.js
+++ public/hangman.controller.client.js
@@ -1,11 +1,11 @@
 (function () {
   angular
   .module("HangmanApp", [])
-  .controller("HangmanController", HangmanController);
+  .controller('HangmanController', HangmanController);
 
-  function HangmanController(HangmanService) {
-
+  function HangmanController() {
     var model = this;
+
     model.message = "Click New Game to begin";
     model.wordArray = ['electric', 'window', 'interesting', 'fortunate', 'enticing', 'telephone', 'paradise', 'measure'];
     model.prevGuesses = "";
@@ -25,52 +25,69 @@
     model.renderHangman = renderHangman;
     model.gameWon = gameWon;
     model.gameLost = gameLost;
+    model.gameIsOver = gameIsOver;
 
     function pickWord() {
       var wordIndex = Math.floor(Math.random() * model.wordArray.length);
       model.theWord = model.wordArray[wordIndex];
-    };
+    }
 
     function initBlanks() {
       model.currentWordState = "";
       for (var i = 0; i < model.theWord.length; i++) {
         model.currentWordState += "_ ";
       }
-    };
+    }
 
     function updateWord() {
-      // if the letter hasn't been guessed yet...
-      if (model.prevGuesses.indexOf(model.guess) == -1 || (model.prevGuesses.length == 0)) {
-        model.prevGuesses += model.guess + ', ';
+      if (!gameIsOver()) {
+        // if the letter hasn't been guessed yet...
+        if (model.prevGuesses.indexOf(model.guess) == -1 || (model.prevGuesses.length == 0)) {
+          if (model.guess != "") {
+            model.prevGuesses += model.guess + ', ';
+          }
+          console.log(model.prevGuesses);
 
-        // if the guessed letter is in the word
-        if (model.theWord.indexOf(model.guess) > -1) {
-          var s = model.currentWordState;
+          // if the guessed letter is in the word
+          if (model.theWord.indexOf(model.guess) > -1) {
+            var s = model.currentWordState;
 
-          for (var i = 0; i < model.theWord.length; i++) {
-            var c = model.theWord.charAt(i);
+            for (var i = 0; i < model.theWord.length; i++) {
+              var c = model.theWord.charAt(i);
 
-            if (c == model.guess) {
-              s = s.substring(0, i * 2) + c + model.currentWordState.substring(i * 2 + 1);
+              if (c == model.guess) {
+                s = s.substring(0, i * 2) + c + model.currentWordState.substring(i * 2 + 1);
+              }
             }
-          }
 
-          model.currentWordState = s;
+            model.currentWordState = s;
+            console.log("currentWordState: " + model.currentWordState);
 
-          // check for game won
-          if (model.currentWordState.indexOf("_") == -1) {
-            gameWon();
-          }
-        } else {
-          // letter is not in the word and hasn't been guessed until now
-          model.wrongGuesses += 1;
-          renderHangman();
+            // check for game won
+            if (gameIsOver()) {
+              gameWon();
+            }
+          } else {
+            // letter is not in the word and hasn't been guessed until now
+            model.wrongGuesses += 1;
+            renderHangman();
 
-          if (model.wrongGuesses == 10) {
-            gameLost();
+            // check for game lost
+            if (gameIsOver()) {
+              gameLost();
+            }
           }
         }
       }
+
+      // update w/ jquery
+      $('#heading').html(model.message);
+      $('#picture').html(model.picture);
+      $('#current-word-state').html(model.currentWordState);
+      $('#prev-guesses').html(model.prevGuesses);
+      $('#wrong-guesses').html(model.wrongGuesses);
+      $('#games-won').html("Games won: " + model.gamesWon);
+      $('#games-lost').html("Games lost: " + model.gamesLost);
     }
 
     function renderHangman() {
@@ -204,25 +221,34 @@
       renderHangman();
       pickWord();
       initBlanks();
-      // TODO ; add unhide upon game start------------------------------------------------------------------------
       model.message = "New Game Started (" + model.theWord + ")";
+
+      $('#heading').html(model.message);
+      $('#picture').html(model.picture);
+      $('#current-word-state').html(model.currentWordState);
+      $('#prev-guesses').html(model.prevGuesses);
+      $('#wrong-guesses').html(model.wrongGuesses);
+      $('#games-won').html("Games won: " + model.gamesWon);
+      $('#games-lost').html("Games lost: " + model.gamesLost);
     }
 
     function gameWon() {
       model.message = "Congratulations, you survived!";
       model.gamesWon += 1;
-      // TODO ; suspend additional guesses ------------------------------------------------------------------------
     }
 
     function gameLost() {
       model.message = "Ouch, you've been hanged!";
       model.gamesLost += 1;
-      // TODO ; suspend additional guesses ------------------------------------------------------------------------
+    }
+
+    function gameIsOver() {
+      return (model.wrongGuesses == 10 || model.currentWordState.indexOf("_") < 0) ? true : false;
     }
 
     function getKeyPressed() {
       var key = event.keyCode;
-      // console.log(key);
+      // console.log("Keycode: " + key);
 
       if (key == 97) {
         model.guess = "a";
@@ -276,10 +302,71 @@
         model.guess = "y";
       } else if (key == 122) {
         model.guess = "z";
-      } else {
-        alert("Please enter a valid letter");
       }
 
+      updateWord();
+    }
+
+    document.onkeydown = function getLetter(evt) {
+      var code = evt.keyCode;
+      console.log("keycode: " + code);
+
+      if (code == 65) {
+        model.guess = "a";
+      } else if (code == 66) {
+        model.guess = "b";
+      } else if (code == 67) {
+        model.guess = "c";
+      } else if (code == 68) {
+        model.guess = "d";
+      } else if (code == 69) {
+        model.guess = "e";
+      } else if (code == 70) {
+        model.guess = "f";
+      } else if (code == 71) {
+        model.guess = "g";
+      } else if (code == 72) {
+        model.guess = "h";
+      } else if (code == 73) {
+        model.guess = "i";
+      } else if (code == 74) {
+        model.guess = "j";
+      } else if (code == 75) {
+        model.guess = "k";
+      } else if (code == 76) {
+        model.guess = "l";
+      } else if (code == 77) {
+        model.guess = "m";
+      } else if (code == 78) {
+        model.guess = "n";
+      } else if (code == 79) {
+        model.guess = "o";
+      } else if (code == 80) {
+        model.guess = "p";
+      } else if (code == 81) {
+        model.guess = "q";
+      } else if (code == 82) {
+        model.guess = "r";
+      } else if (code == 83) {
+        model.guess = "s";
+      } else if (code == 84) {
+        model.guess = "t";
+      } else if (code == 85) {
+        model.guess = "u";
+      } else if (code == 86) {
+        model.guess = "v";
+      } else if (code == 87) {
+        model.guess = "w";
+      } else if (code == 88) {
+        model.guess = "x";
+      } else if (code == 89) {
+        model.guess = "y";
+      } else if (code == 90) {
+        model.guess = "z";
+      }
+
+      // console.log("Key is: " + model.guess);
+      console.log("model.guess is: " + model.guess);
       updateWord();
     }
   }
