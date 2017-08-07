2643afeace367a9bd19bfcc1ec3d35477e9cc29e
Jeremy Stern <jzstern@gmail.com>
2017-08-07 00:44:56 -0400

    url redirect

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+});
 
 app.listen(process.env.PORT || 3000);
 // app.listen(3000);
